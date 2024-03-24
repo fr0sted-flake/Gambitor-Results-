f6fb3a0bfb2bc6576322a9cd015fbb462ba299b4
Ayush Bhardwaj <fr0sted.flake.02@gmail.com>
2024-03-24 14:19:30 +0530

    modified tailwind config and also navbar

--- src/_components/common/Header/Header.jsx
+++ src/_components/common/Header/Header.jsx
@@ -1,11 +1,54 @@
 "use client";
-import Navbar from "@/_components/common/Navbar/Navbar";
+import styles from "./Header.module.css";
 import { usePathname, useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import horseLogo from "@/images/common/header/horse.png";
+import gambitor from "@/images/common/header/gambitor-letters.png";
 
 const Header = () => {
   return (
-    <div className="h-16 fixed w-full top-0 z-[100] transition-bg duration-400 ease-in-out"></div>
+    <header>
+      <div
+        id="navbar"
+        className="h-16vh w-full pt-3vh fixed z-50 flex items-start justify-between transition-colors duration-300 ease-in-out"
+      >
+        <div className="ml-5vw w-20vw h-13vh flex items-center">
+          <div className="relative w-3.5vw m-0 mb-12">
+            <Link href="/">
+              <Image className={styles.img1} src={horseLogo} />
+            </Link>
+          </div>
+
+          <Link href="/">
+            <Image src={gambitor} className={styles.img2} />
+          </Link>
+        </div>
+        <ul className=" flex justify-evenly items-center mx-1.5vw mb-16 h-80">
+          <li>
+            <Link href="about" className={styles.tags}>
+              ABOUT
+            </Link>
+          </li>
+          <li>
+            <Link href="events" className={styles.tags}>
+              EVENTS
+            </Link>
+          </li>
+          <li>
+            <Link href="contact" className={styles.tags}>
+              CONTACT
+            </Link>
+          </li>
+          <li>
+            <Link href="results" className={styles.tags}>
+              RESULTS
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </header>
   );
 };
 
